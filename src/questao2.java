d00e34a8f4be679866ab1fafc9958e949c2546aa
Thiago Altieri Berbel da Silva <altierithiago28@gmail.com>
2023-12-15 21:23:20 -0300

    Final

--- src/questao2.java
+++ src/questao2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class qestao2v2 {
+public class questao2 {
 
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
